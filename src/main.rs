d58b65f8aa23593e7665e53ca6b009a36cca5068
Nicolas Patry <patry.nicolas@protonmail.com>
2022-07-10 01:42:47 +0200

    Putting back full bloom config on.

--- src/main.rs
+++ src/main.rs
@@ -1305,9 +1305,9 @@ async fn main() -> std::io::Result<()> {
     let (s13, r13) = bounded::<Msg2>(1);
     let (s14, r14) = bounded::<Msg2>(1);
 
-    let (is_350m, config_fn, layout_config_fn) =
-        (true, || Config::new350m(), || LayoutConfig::new350m());
-    // let (is_350m, config) = (false, LayoutConfig::new());
+    // let (is_350m, config_fn, layout_config_fn) =
+    //     (true, || Config::new350m(), || LayoutConfig::new350m());
+    let (is_350m, config_fn, layout_config_fn) = (false, || Config::newm(), || LayoutConfig::new());
 
     if is_350m {
         std::thread::spawn(move || {
