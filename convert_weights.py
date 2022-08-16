62c127b4a04dac9c28e0364bfcf0f1b5c6665402
Your Name <you@example.com>
2022-08-16 13:59:39 +0000

    Update convert_weight to newer transformers

--- convert_weights.py
+++ convert_weights.py
@@ -1,25 +1,14 @@
 from safetensors.torch import save_file
 # from huggingface_hub import hf_hub_download
-from transformers.utils import hf_bucket_url, cached_path
+from transformers.utils import cached_file
 import torch
 import tqdm
 import os
 
 DIRECTORY = "weights"
 
-def hf_hub_download(model_id, filename):
-    shard_url = hf_bucket_url(
-            model_id,
-            filename=filename,
-        )
-    cached_filename = cached_path(
-            shard_url,
-    )
-    return cached_filename
-
-
 def convert_350m():
-    filename = hf_hub_download("bigscience/bloom-560m", filename="pytorch_model.bin")
+    filename = cached_file("bigscience/bloom-560m", filename="pytorch_model.bin")
     data = torch.load(filename, map_location="cpu")
 
     # Need to copy since that call mutates the tensors to numpy
@@ -27,7 +16,7 @@ def convert_350m():
 
 
 def convert_testing():
-    filename = hf_hub_download(
+    filename = cached_file(
         "bigscience/bigscience-small-testing", filename="pytorch_model.bin"
     )
     data = torch.load(filename, map_location="cpu")
@@ -51,7 +40,7 @@ def convert_full():
     ):
         if os.path.exists(local):
             continue
-        filename = hf_hub_download(MODEL_ID, filename=filename)
+        filename = cached_file(MODEL_ID, filename=filename)
         data = torch.load(filename, map_location="cpu")
 
         save_file(data.copy(), os.path.join(DIRECTORY, local))
